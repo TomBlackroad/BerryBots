98cadc62295e1929c88cfe8e29210993a3251b73
TomBlackroad <mbl.7d@live.com>
2020-09-05 01:12:11 +0200

    deleted files and folders2

--- ArduPi/newtest.py
+++
@@ -1,8 +0,0 @@
-import serial, time
-arduino = serial.Serial("/dev/ttyAMA0", 9600)
-print("setup done")
-time.sleep(2)
-print("sleep done")
-arduino.write(b'9')
-print("write done")
-arduino.close()
